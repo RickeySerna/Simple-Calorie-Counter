be6e5f96ae62de66404b51a41b678e0737dd817d
RickeySerna <rickeyserna7@live.com>
2024-12-08 18:08:48 -0600

    Added utils.py Moved all helper function to that file.

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from decimal import Decimal, getcontext
 from models import db
 from controllers import food_item_bp
+from utils import *
 
 app = Flask(__name__)
 # Specifying the domain (currently just localhost) we'll be receiving request from to avoid CORS issues.
@@ -18,54 +18,6 @@ app.register_blueprint(food_item_bp)
 
 with app.app_context():
     db.create_all()
-
-def to_float(value, default=0.0):
-    try:
-        return float(value)
-    except (TypeError, ValueError):
-        return default
-
-# Using Decimal to avoid any long floating points where they don't belong.
-## For example, if a computation should come out to 58.0, it can become 57.99999999 using normal floats. Decimal avoids that.
-def to_decimal(value, default=Decimal('0.0')):
-    try:
-        # Checking if the var is empty to avoid conversion errors.
-        if value is None or value == '':
-            return default
-        return Decimal(value)
-    except (TypeError, ValueError, InvalidOperation):
-        return default
-
-# Formula to convert food weight and serving size form whatever unit the user used to grams.
-def convert_to_grams(weight: Decimal, unit: str, ounces: Decimal = Decimal('0.0')) -> Decimal:
-    # UPDATE: The function now uses a dictionary to pull the conversion rate.
-    conversion_factors = {
-        "kg": Decimal("1000"),
-        "oz": Decimal("28.3495"),
-        "lb": Decimal("453.592"),
-        "mL": Decimal("1"),
-        "mg": Decimal("0.001")
-    }
-    # For most units, we just return weight * the conversion factor passed on the function call.
-    if unit in conversion_factors:
-        return weight * conversion_factors[unit]
-    # Lbs and oz is a slightly special case so we call those explicitly here.
-    elif unit == "lb_oz":
-        return (weight * conversion_factors["lb"]) + (ounces * conversion_factors["oz"])
-    # This is triggered is the unit is "g" as that is not in conversion_factors. In that case, just return the weight.
-    else:
-        return weight
-
-def format_weight(weight):
-    weight_str = str(weight).rstrip('0').rstrip('.')
-    return weight_str if '.' in weight_str else str(int(weight))
-
-def format_macros(macro: Decimal) -> str:
-    rounded_macro = str(round(macro, 2))
-    if rounded_macro.endswith('.00'):
-        return str(int(macro))
-    else:
-        return rounded_macro.rstrip('0').rstrip('.')
 
 @app.route('/api/calories', methods=['POST'])
 def calculate_macros():
