50cdbe16be87e5072dd7cb0f660b2c10ba2ae6a0
RickeySerna <rickeyserna7@live.com>
2024-12-31 20:40:31 -0600

    Updated FoodItem DB schema FoodItems now separate between the weight value and weight unit.

--- backend/models.py
+++ backend/models.py
@@ -8,17 +8,19 @@ class FoodItem(db.Model):
     date = db.Column(db.Date, nullable=False, default=date.today)
     name = db.Column(db.String(100), nullable=False)
     sub_description = db.Column(db.String(200))
-    weight = db.Column(db.String(100), nullable=False)
+    weight_value = db.Column(db.Float, nullable=False)
+    weight_unit = db.Column(db.String(10), nullable=False)
     calories = db.Column(db.Float, nullable=False)
     protein = db.Column(db.Float, nullable=False)
     carbs = db.Column(db.Float, nullable=False)
     fat = db.Column(db.Float, nullable=False)
 
-    def __init__(self, date, name, sub_description, weight, calories, protein, carbs, fat):
+    def __init__(self, date, name, sub_description, weight_value, weight_unit, calories, protein, carbs, fat):
         self.date = date
         self.name = name
         self.sub_description = sub_description
-        self.weight = weight
+        self.weight_value = weight_value
+        self.weight_unit = weight_unit
         self.calories = calories
         self.protein = protein
         self.carbs = carbs
