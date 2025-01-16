b87481970a99039ec7d47c26a6ec4e214479a8f9
RickeySerna <rickeyserna7@live.com>
2025-01-16 12:55:44 -0600

    Refactored weight conversion method

--- backend/models.py
+++ backend/models.py
@@ -40,7 +40,7 @@ class FoodItem(db.Model):
             self.protein = protein
             self.carbs = carbs
             self.fat = fat
-        
+
         @staticmethod
         def calculate_macros(data):
             print("Data from frontend in calculate_macros: ", data)
@@ -66,17 +66,19 @@ class FoodItem(db.Model):
             serving_size_ounces = to_decimal(data.get('servingSizeOunces'))
 
             # Convert weight and serving size to grams.
-            if weight_unit == 'lb_oz':
-                weight_in_grams = convert_to_grams(weight_pounds, weight_unit, weight_ounces)
-                print(f"weight_in_grams in lboz flow: {weight_in_grams}")
-            else:
-                weight_in_grams = convert_to_grams(weight, weight_unit)
-                print("WE ARE NOT IN LBOZ FLOW")
+            weights_in_grams = FoodItem.Macros.convert_to_grams(data)
 
-            if serving_size_unit == 'lb_oz':
-                serving_size_in_grams = convert_to_grams(serving_size_pounds, serving_size_unit, serving_size_ounces)
-            else:
-                serving_size_in_grams = convert_to_grams(serving_size, serving_size_unit)
+            # if weight_unit == 'lb_oz':
+            #     weight_in_grams = convert_to_grams(weight_pounds, weight_unit, weight_ounces)
+            #     print(f"weight_in_grams in lboz flow: {weight_in_grams}")
+            # else:
+            #     weight_in_grams = convert_to_grams(weight, weight_unit)
+            #     print("WE ARE NOT IN LBOZ FLOW")
+
+            # if serving_size_unit == 'lb_oz':
+            #     serving_size_in_grams = convert_to_grams(serving_size_pounds, serving_size_unit, serving_size_ounces)
+            # else:
+            #     serving_size_in_grams = convert_to_grams(serving_size, serving_size_unit)
 
             # Making sure the calculation variables are in Decimal too.
             weight_in_grams = to_decimal(weight_in_grams)
@@ -127,3 +129,44 @@ class FoodItem(db.Model):
                 carbs=formatted_net_carbs,
                 fat=formatted_fat
             )
+        
+        # Formula to convert food weight and serving size form whatever unit the user used to grams.
+        @staticmethod
+        def convert_to_grams(data):
+
+            #weight: Decimal, unit: str, ounces: Decimal = Decimal('0.0')) -> Decimal:
+
+            # UPDATE: The function now uses a dictionary to pull the conversion rate.
+            conversion_factors = {
+                "kg": Decimal("1000"),
+                "oz": Decimal("28.3495"),
+                "lb": Decimal("453.592"),
+                "mL": Decimal("1"),
+                "mg": Decimal("0.001")
+            }
+
+            # For most units, we just return weight * the conversion factor passed on the function call.
+            if data.get('weightUnit') in conversion_factors:
+                weight_in_grams = data.get('weight') * conversion_factors[data.get('weightUnit')]
+            # Lbs and oz is a slightly special case so we call those explicitly here.
+            elif data.get('weightUnit') == "lb_oz":
+                weight_in_grams = (data.get('weightPounds') * conversion_factors["lb"]) + (data.get('weightOunces') * conversion_factors["oz"])
+            # This is triggered is the unit is "g" as that is not in conversion_factors. In that case, just return the weight.
+            else:
+                weight_in_grams = data.get('weight')
+
+            if data.get('servingSizeUnit') in conversion_factors:
+                serving_size_in_grams = data.get('servingSize') * conversion_factors[data.get('servingSizeUnit')]
+            elif data.get('servingSizeUnit') == "lb_oz":
+                serving_size_in_grams = (data.get('servingSizePounds') * conversion_factors["lb"]) + (data.get('servingSizeOunces') * conversion_factors["oz"])
+            else:
+                serving_size_in_grams = data.get('servingSize')
+
+            weights = {
+                "weight_in_grams": weight_in_grams,
+                "serving_size_in_grams": serving_size_in_grams
+            }
+
+            print(f"weights being returned in convert_to_grams in model: {weights}")
+
+            return weights
